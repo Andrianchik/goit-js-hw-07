42b17c83e2365d968699ea263fd5069d87d92882
Andrianchik <oksenenkoandrij@gmaile.com>
2024-10-08 20:40:15 +0300

    1

--- js/task-6.js
+++ js/task-6.js
@@ -3,30 +3,59 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const input = document.querySelector('input');
-const createButton = document.querySelector('[data-create]');
-const destroyButton = document.querySelector('[data-destroy]');
+
+
+
+
+const controls = document.querySelector('#controls');
+const input = controls.querySelector('input');
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
 const boxesContainer = document.querySelector('#boxes');
-createButton.addEventListener('click', createBoxes);
-destroyButton.addEventListener('click', destroyBoxes);
-function createBoxes() {
-const amount = input.value;
-if (amount < 1 || amount > 100) {
-    alert('Please enter a number between 1 and 100.');
-    return;
-}
-destroyBoxes();
-let size = 30;
-for (let i = 0; i < amount; i++) {
+
+
+function createBoxes(amount) {
+  
+  boxesContainer.innerHTML = '';
+
+ 
+  const boxes = [];
+  let size = 30; 
+
+  for (let i = 0; i < amount; i++) {
     const box = document.createElement('div');
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
-    size += 10;
+    box.style.marginTop = '10px';
+    boxes.push(box); 
+    size += 10; 
+  }
+
+ 
+  boxesContainer.append(...boxes);
 }
-input.value = '';
-}
+
+
 function destroyBoxes() {
-boxesContainer.innerHTML = '';
+  boxesContainer.innerHTML = ''; 
 }
+
+
+createButton.addEventListener('click', () => {
+  const amount = Number(input.value);
+
+  
+  if (amount < 1 || amount > 100 || isNaN(amount)) {
+    alert('Please enter a number between 1 and 100');
+    return;
+  }
+
+  createBoxes(amount); 
+  input.value = ''; 
+});
+
+
+destroyButton.addEventListener('click', () => {
+  destroyBoxes(); 
+});
